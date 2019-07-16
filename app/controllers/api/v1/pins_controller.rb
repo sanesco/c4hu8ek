87a9a71d0d8a72f1f25ee74092e559ad79f2d820
sanesco <santiagoescobar11z@gmail.com>
2019-07-16 11:21:54 -0500

    Add authotization for API

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::PinsController < ApplicationController
+  before_action :authenticate
+
   def index
     render json: Pin.all.order('created_at DESC')
   end
-
   def create
     pin = Pin.new(pin_params)
     if pin.save
@@ -11,9 +12,14 @@ class Api::V1::PinsController < ApplicationController
       render json: { errors: pin.errors }, status: 422
     end
   end
-
   private
     def pin_params
       params.require(:pin).permit(:title, :image_url)
+    end
+
+    def authenticate
+      email_auth = request.headers["X-User-Email"]
+      token_auth = request.headers["X-Api-Token"]
+      head 401 unless User.find_by_email(email_auth) && User.find_by_api_token(token_auth)
     end
 end
